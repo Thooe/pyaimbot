0216c7ba6e5f238ea49322d33ce5d655dffaa679
Thooe <fudeiq@o2.pl>
2017-06-03 11:45:04 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+import win32api
+#for i in pyautogui.getWindows():
+#	print(i)
+print("Starting in 3 seconds...")
+time.sleep(3)
+found = 0
+notfound = 0
+while(True):
+
+	stuff = pyautogui.locateOnScreen('pixels/FF0013.png', region=(620, 320, 150, 170))
+	if stuff == None:
+		stuff = pyautogui.locateOnScreen('pixels/FF0012.png', region=(620, 320, 150, 170))
+		if stuff == None:
+			stuff = pyautogui.locateOnScreen('pixels/FF0014.png', region=(620, 320, 150, 170))
+			if stuff != None:
+				x, y = pyautogui.center(stuff)
+				found = found + 1
+				print(str(x)+" "+str(y))
+				win32api.SetCursorPos((x,y))
+				time.sleep(1)
+			else: notfound = notfound + 1
+	print("Found: "+str(found)+" Not Found:"+str(notfound))
+#600-320
+
+#def coolStart(howLong, text):
+#	while howLong>0:
+#		print("Starting in {howLong} seconds...".format(int(howLong))
+#		howLong = howLong-1
